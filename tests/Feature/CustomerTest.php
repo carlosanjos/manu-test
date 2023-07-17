a6508020165c7d06c6b1e38656a92430acf82a0e
Carlos Anjos <carlos@goodtravelsoftware.com>
2023-07-17 21:47:11 +0100

    test commit

---
+++ tests/Feature/CustomerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CustomerTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     */
+    public function testCustomerChunkedUpdated(): void
+    {
+        $customers = \App\Models\Customer::factory(1000)->create();
+
+        $this->assertCount(1000, $customers);
+
+        $customersChunk = $customers->chunk(100);
+
+        foreach ($customersChunk as $customer) {
+            $customer->each->update(['name' => 'pong']);
+        }
+
+        $this->assertCount(1000, $customers);
+        $this->assertDatabaseHas('customers', ['name' => 'pong']);
+    }
+
+    /**
+     * Don't work for some reason.
+     */
+    public function testCustomerChunkedUpdatedClosure(): void
+    {
+        $customers = \App\Models\Customer::factory(1000)->create();
+
+        $this->assertCount(1000, $customers);
+
+        $customers->chunk(100)->map(function ($customerChunk) {
+            $customerChunk->each->update(["name"=> "pong"]);
+        });
+
+        $this->assertCount(1000, $customers);
+        $this->assertDatabaseHas('customers', ['name' => 'pong']);
+    }
+}
